6b79fc5ca9423a646602d2237c688c502f44d29f
dyu <david.yu.ftw@gmail.com>
2022-02-13 01:43:53 +0800

    dist dir

--- dist.sh
+++ dist.sh
@@ -6,8 +6,9 @@ set -e
 
 DIR=cosmopolitan
 
-mkdir -p $DIR
-cp o/ape/ape.lds o/ape/ape.o o/libc/crt/crt.o o/cosmopolitan.a o/cosmopolitan.h $DIR
+mkdir -p dist/$DIR
+cp o/ape/ape.lds o/ape/ape.o o/libc/crt/crt.o o/cosmopolitan.a o/cosmopolitan.h dist/$DIR
+cd dist
 [ -e $DIR.tar.gz ] && rm $DIR.tar.gz
 tar -cvzf $DIR.tar.gz $DIR
 
