a18d453f15c925351d250279de44e792b81c2ffd
dyu <david.yu.ftw@gmail.com>
2022-08-25 00:24:32 +0800

    gcc from nix if available

--- dist.sh
+++ dist.sh
@@ -16,25 +16,32 @@ mkdir -p dist/$DIR
 cp $REL_DIR/ape/ape.lds $REL_DIR/ape/ape.o $REL_DIR/ape/ape-no-modify-self.o $REL_DIR/ape/ape-copy-self.o $REL_DIR/libc/crt/crt.o $REL_DIR/cosmopolitan.a o/cosmopolitan.h dist/$DIR
 
 # build example
-EXAMPLE=hello3
+EXAMPLE=nomodifyself
 [ -n "$2" ] && EXAMPLE=$2
 echo "Compiling examples/$EXAMPLE.c to dist/$EXAMPLE.com"
 
-#gcc -g -Os -static -fno-pie -no-pie -mno-red-zone -nostdlib -nostdinc \
+GCC=gcc
+OBJCOPY=objcopy
+
+BIN_DIR=/nix/store/kia8xygy2r9iblwbjrl163j4bpz84wz7-gcc-wrapper-11.3.0/bin
+[ -e $BIN_DIR ] && GCC=$BIN_DIR/gcc && OBJCOPY=$BIN_DIR/objcopy && echo "Using gcc and objcopy from $BIN_DIR"
+
+#$GCC -g -Os -static -fno-pie -no-pie -mno-red-zone -nostdlib -nostdinc \
 #  -fno-omit-frame-pointer -pg -mnop-mcount \
 #  -o dist/hello3.com.dbg examples/hello3.c \
 #  -Wl,--gc-sections -fuse-ld=bfd -Wl,-T,dist/$DIR/ape.lds \
 #  -include dist/$DIR/cosmopolitan.h -I. \
 #  dist/$DIR/crt.o dist/$DIR/ape.o dist/$DIR/cosmopolitan.a
 
-gcc -g -Os -static -nostdlib -nostdinc -fno-pie -no-pie -mno-red-zone \
+$GCC -g -Os -static -nostdlib -nostdinc -fno-pie -no-pie -mno-red-zone \
   -fno-omit-frame-pointer -pg -mnop-mcount -mno-tls-direct-seg-refs \
   -o dist/$EXAMPLE.com.dbg examples/$EXAMPLE.c -fuse-ld=bfd -Wl,-T,dist/$DIR/ape.lds -Wl,--gc-sections \
   -include dist/$DIR/cosmopolitan.h -I. \
   dist/$DIR/crt.o dist/$DIR/ape-no-modify-self.o dist/$DIR/cosmopolitan.a
 
-objcopy -S -O binary dist/$EXAMPLE.com.dbg dist/$EXAMPLE.com
+$OBJCOPY -S -O binary dist/$EXAMPLE.com.dbg dist/$EXAMPLE.com
 
 cd dist
 [ -e $DIR.tar.gz ] && rm $DIR.tar.gz
 tar -cvzf $DIR.tar.gz $DIR
+
