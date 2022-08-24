5f26179167c244c61c0caee09d5a6b5f698d6030
dyu <david.yu.ftw@gmail.com>
2022-08-25 01:19:57 +0800

    default to static gcc

--- dist.sh
+++ dist.sh
@@ -20,18 +20,11 @@ EXAMPLE=nomodifyself
 [ -n "$2" ] && EXAMPLE=$2
 echo "Compiling examples/$EXAMPLE.c to dist/$EXAMPLE.com"
 
-GCC=gcc
-OBJCOPY=objcopy
+GCC=./o/third_party/gcc/bin/x86_64-linux-musl-gcc
+OBJCOPY=./o/third_party/gcc/bin/x86_64-linux-musl-objcopy
 
-BIN_DIR=/nix/store/kia8xygy2r9iblwbjrl163j4bpz84wz7-gcc-wrapper-11.3.0/bin
-[ -e $BIN_DIR ] && GCC=$BIN_DIR/gcc && OBJCOPY=$BIN_DIR/objcopy && echo "Using gcc and objcopy from $BIN_DIR"
-
-#$GCC -g -Os -static -fno-pie -no-pie -mno-red-zone -nostdlib -nostdinc \
-#  -fno-omit-frame-pointer -pg -mnop-mcount \
-#  -o dist/hello3.com.dbg examples/hello3.c \
-#  -Wl,--gc-sections -fuse-ld=bfd -Wl,-T,dist/$DIR/ape.lds \
-#  -include dist/$DIR/cosmopolitan.h -I. \
-#  dist/$DIR/crt.o dist/$DIR/ape.o dist/$DIR/cosmopolitan.a
+#BIN_DIR=/nix/store/kia8xygy2r9iblwbjrl163j4bpz84wz7-gcc-wrapper-11.3.0/bin
+#[ -e $BIN_DIR ] && GCC=$BIN_DIR/gcc && OBJCOPY=$BIN_DIR/objcopy && echo "Using gcc and objcopy from $BIN_DIR"
 
 $GCC -g -Os -static -nostdlib -nostdinc -fno-pie -no-pie -mno-red-zone \
   -fno-omit-frame-pointer -pg -mnop-mcount -mno-tls-direct-seg-refs \
